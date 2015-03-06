a4f533f136b8d1b7d6776c2b17dc62197cf2d272
Semyon Burov <ilionsd@gmail.com>
2015-03-06 13:20:35 +0300

    Идея с репараметризацией с помощью подстановки на самом деле была не очень

--- src/com/insign/insign/MainActivity.java
+++ src/com/insign/insign/MainActivity.java
@@ -10,11 +10,16 @@ import android.view.MotionEvent;
 import android.view.View;
 
 import android.widget.TextView;
-import com.insign.common.function.Spline;
-import com.insign.common.function.Interpolation;
+import com.insign.common.Entry;
+import com.insign.common.EntryWrapper;
+import com.insign.common.function.differentialgeometry.NaturalSplineParametricCurve;
+import com.insign.common.function.differentialgeometry.ParametricCurve;
+import com.insign.common.function.differentialgeometry.SplineParametricCurve;
+import com.insign.common.function.interpolation.Interpolation;
 import com.insign.common.function.Point2D;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by ilion on 06.02.2015.
@@ -25,9 +30,7 @@ public class MainActivity extends Activity implements View.OnTouchListener{
 	View view;
 	DrawView drawView;
 	Paint paint = new Paint();
-	ArrayList<Point2D> coords;
-	ArrayList<Point2D> xt;
-	ArrayList<Point2D> yt;
+	List<Entry<Double, Point2D>> points;
 	long t0 = 0;
 
 
@@ -54,51 +57,22 @@ public class MainActivity extends Activity implements View.OnTouchListener{
 				break;
 			case MotionEvent.ACTION_MOVE:
 				addPoint(event.getX(), event.getY(), event.getEventTime() - t0);
-				int lastIndex = coords.size() - 1;
-				float startX = (float)coords.get(lastIndex - 1).getX();
-				float startY = (float)coords.get(lastIndex - 1).getY();
-				float stopX = (float)coords.get(lastIndex).getX();
-				float stopY = (float)coords.get(lastIndex).getY();
-				paint.setColor(Color.WHITE);
-				paint.setStrokeWidth(10);
-				drawView.getCanvas().drawLine(startX, startY, stopX, stopY, paint);
-				drawView.invalidate();
+				moveEventActionMove();
 				break;
 			case MotionEvent.ACTION_UP:
-				if (coords.size() < 10)
+				if (points.size() < 10)
 					break;
-				Point2D[] xtArr = xt.toArray(new Point2D[xt.size()]);
-				Point2D[] ytArr = yt.toArray(new Point2D[yt.size()]);
- 				long start = System.currentTimeMillis();
-				Spline splineX = Interpolation.SmoothingSpline(xtArr, 0.5);
-				Spline splineY = Interpolation.SmoothingSpline(ytArr, 0.5);
-				long execTime = System.currentTimeMillis() - start;
-
-				Spline.isContinuous(splineX, 1e-10);
-				Spline.isContinuous(splineY, 1e-10);
-
-				drawSpline(splineX, splineY, 1);
-				paint.setColor(Color.RED);
-				paint.setTextSize(70);
-				//drawView.getCanvas().drawColor(Color.WHITE);
-				drawView.getCanvas().drawText(Double.toString(execTime), 100.0F, 100.0F, paint);
-				drawView.invalidate();
+				moveEventActionUp();
 				break;
 		}
 		return true;
 	}
 
 	private void initialize() {
-		if (coords == null)
-			coords = new ArrayList<Point2D>();
-		if (xt == null)
-			xt = new ArrayList<Point2D>();
-		if (yt == null)
-			yt = new ArrayList<Point2D>();
+		if (points == null)
+			points = new ArrayList<Entry<Double, Point2D>>();
 
-		coords.clear();
-		xt.clear();
-		yt.clear();
+		points.clear();
 
 		textView.setText("List View");
 		drawView.getCanvas().drawColor(Color.TRANSPARENT, PorterDuff.Mode.CLEAR);
@@ -106,27 +80,54 @@ public class MainActivity extends Activity implements View.OnTouchListener{
 	}
 
 	private void addPoint(double x, double y, long t) {
-		coords.add(new Point2D(x, y));
-		xt.add(new Point2D(t, x));
-		yt.add(new Point2D(t, y));
+		points.add(new EntryWrapper<Double, Point2D>((double) t, new Point2D(x, y)));
 		textView.append(System.lineSeparator() + "t=" + t + "(" + x + "; " + y + ")");
 	}
 
-	private void drawSpline(Spline x, Spline y, double step) {
-		double t0 = x.getLeftBound();
-		double tMax = x.getRightBound();
+	private void drawCurve(ParametricCurve curve, double step, int color) {
+		double tMin = curve.getParameterMin();
+		double tMax = curve.getParameterMax();
 		double t1 = 0, t2 = 0;
-		double segmentSize = tMax - t0;
+		double segmentSize = tMax - tMin;
 		int segmentCount = (int)Math.ceil(segmentSize / step);
 		double actualStep = segmentSize / segmentCount;
 
-		paint.setColor(Color.RED);
+		paint.setColor(color);
 		paint.setStrokeWidth(3);
 		for (int segment = 0; segment < segmentCount; segment++) {
-			t1 = t0 + segment * actualStep;
+			t1 = tMin + segment * actualStep;
 			t2 = t1 + actualStep;
-			drawView.getCanvas().drawLine((float)x.valueIn(t1), (float)y.valueIn(t1), (float)x.valueIn(t2), (float)y.valueIn(t2), paint);
+			Point2D t1Value = curve.valueIn(t1);
+			Point2D t2Value = curve.valueIn(t2);
+			drawView.getCanvas().drawLine((float)t1Value.getX(), (float)t1Value.getY(), (float)t2Value.getX(), (float)t2Value.getY(), paint);
 		}
+	}
+
+	private void moveEventActionMove() {
+		int lastIndex = points.size() - 1;
+		float startX = (float)points.get(lastIndex - 1).getValue().getX();
+		float startY = (float)points.get(lastIndex - 1).getValue().getY();
+		float stopX = (float)points.get(lastIndex).getValue().getX();
+		float stopY = (float)points.get(lastIndex).getValue().getY();
+		paint.setColor(Color.WHITE);
+		paint.setStrokeWidth(10);
+		drawView.getCanvas().drawLine(startX, startY, stopX, stopY, paint);
+		drawView.invalidate();
+	}
+
+	private void moveEventActionUp() {
+		long start = System.currentTimeMillis();
+		SplineParametricCurve curve = Interpolation.ParametricCurves.bySmoothingSpline(points, 0.5);
+		NaturalSplineParametricCurve naturalCurve = NaturalSplineParametricCurve.fromCurve(curve);
+		long execTime = System.currentTimeMillis() - start;
+
+		drawCurve(curve, 1, Color.RED);
+		drawCurve(naturalCurve, 1, Color.GREEN);
+		paint.setColor(Color.RED);
+		paint.setTextSize(70);
+		//drawView.getCanvas().drawColor(Color.WHITE);
+		drawView.getCanvas().drawText(Double.toString(execTime), 100.0F, 100.0F, paint);
+		drawView.invalidate();
 	}
 
 }
